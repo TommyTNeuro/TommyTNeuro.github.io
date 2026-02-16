1c66b67df8b43e6d280d41db80ae2addfdb56d9d
TommyTNeuro <TommyTKRoberts@gmail.com>
2026-02-16 01:36:13 +0000

    Deploying to gh-pages from @ TommyTNeuro/TommyTNeuro.github.io@b9522c5c30016166386edc37d0d9063e82643e16 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,26 +9,12 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-news",
-          title: "news",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/news/";
-          },
-        },{id: "nav-blog",
+  },{id: "nav-blog",
           title: "blog",
           description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/blog/";
-          },
-        },{id: "nav-repositories",
-          title: "repositories",
-          description: "This is a collection of my repos that I&#39;ve worked on and GitHub stats.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/repositories/";
           },
         },{id: "nav-cv",
           title: "cv",
@@ -37,6 +23,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
+        },{id: "nav-news",
+          title: "news",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/news/";
+          },
         },{id: "nav-teaching",
           title: "teaching",
           description: "Materials for courses you taught. Replace this text with your description.",
@@ -44,70 +37,10 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/teaching/";
           },
-        },{id: "post-why-you-brain-loses-steam",
-        
-          title: "Why you brain loses steam",
-        
-        description: "A brief introduction to cognitive load theory",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2026/Why-your-brain-runs-out-of-stream/";
-          
-        },
-      },{id: "news-joined-the-the-british-neuroscience-association-and-will-be-attending-the-2026-british-neuroscience-association-meeting",
+        },{id: "news-joined-the-the-british-neuroscience-association-and-will-be-attending-the-2026-british-neuroscience-association-meeting",
           title: 'Joined the the British Neuroscience Association and will be attending the 2026 British...',
           description: "",
           section: "News",},{
-        id: 'social-cv',
-        title: 'CV',
-        section: 'Socials',
-        handler: () => {
-          window.open("/assets/pdf/Thomas-Roberts_Academic_CV.pdf", "_blank");
-        },
-      },{
-        id: 'social-email',
-        title: 'email',
-        section: 'Socials',
-        handler: () => {
-          window.open("mailto:%74%6F%6D%6D%79%74%6B%72%6F%62%65%72%74%73@%67%6D%61%69%6C.%63%6F%6D", "_blank");
-        },
-      },{
-        id: 'social-github',
-        title: 'GitHub',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://github.com/TommyTNeuro", "_blank");
-        },
-      },{
-        id: 'social-linkedin',
-        title: 'LinkedIn',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.linkedin.com/in/TommyTNeuro", "_blank");
-        },
-      },{
-        id: 'social-medium',
-        title: 'Medium',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://medium.com/@TommyTNeuro", "_blank");
-        },
-      },{
-        id: 'social-orcid',
-        title: 'ORCID',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://orcid.org/0009-0004-3158-0578", "_blank");
-        },
-      },{
-        id: 'social-osf',
-        title: 'Open Science Framework',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://osf.io/btgz2/", "_blank");
-        },
-      },{
       id: 'light-theme',
       title: 'Change theme to light',
       description: 'Change the theme of the site to Light',
